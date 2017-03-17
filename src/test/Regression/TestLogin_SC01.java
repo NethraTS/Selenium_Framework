46053464dc3b1f82a0e4f4c121cdfd6bc2de9ff9
Nethra <nethra.suthagar@terralogic.com>
2017-03-17 17:12:50 +0530

    Modified Test code

--- src/test/Regression/TestLogin_SC01.java
+++ src/test/Regression/TestLogin_SC01.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import junit.framework.Assert;
 import ui.HomePage;
 import ui.LoginPage;
